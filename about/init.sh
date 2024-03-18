42cdb99f83e4e7e88aa895c7a03ef8072f41ceed
Eric Mignot <eric.mignot@gmail.com>
2024-03-18 10:44:37 -0700

    no tty

--- about/init.sh
+++ about/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function test_init {
-    docker run -i -t -v $(pwd)/services:/usr/local/src hashicorp/terraform:1.5.7 -chdir=/usr/local/src init > output
+    docker run -i -v $(pwd)/services:/usr/local/src hashicorp/terraform:1.5.7 -chdir=/usr/local/src init > output
     local ok=`cat output | grep "Terraform has been successfully initialized" | wc -l`
     
     assertequals $ok 1
