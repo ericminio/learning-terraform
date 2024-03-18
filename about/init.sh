ba713210160847b4c2c091caa602707ecc8a2568
Eric Mignot <eric.mignot@gmail.com>
2024-03-18 13:54:35 -0700

    about apply

--- about/init.sh
+++ about/init.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+function clean {
+    rm -rf $(pwd)/services/.terraform
+    rm $(pwd)/services/.terraform*
+    rm $(pwd)/services/terraform*
+}
+
 function test_init {
+    clean
     docker run -i -v $(pwd)/services:/usr/local/src hashicorp/terraform:1.5.7 -chdir=/usr/local/src init > output
     local ok=`cat output | grep "Terraform has been successfully initialized" | wc -l`
     
     assertequals $ok 1
+}
+
+function test_apply {
+    clean
+    docker run -i -v $(pwd)/services:/usr/local/src hashicorp/terraform:1.5.7 -chdir=/usr/local/src init > output
+    docker run -i -v $(pwd)/services:/usr/local/src hashicorp/terraform:1.5.7 -chdir=/usr/local/src apply --auto-approve >> output
+    local ok=`cat output | grep "Hello World" | wc -l`
+    
+    assertequals $ok 2
 }
